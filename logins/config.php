dc2467909cfe8d7f4c4e86e4975f1d026f573720
cpbasto98 <cpbasto98@gmail.com>
2025-11-30 21:01:27 +0800

    1

---
+++ logins/config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'barangay_rizal',
+    'db_user' => 'root',
+    'db_pass' => 'admin1234', // put your MySQL password here (XAMPP default is empty)
+];
